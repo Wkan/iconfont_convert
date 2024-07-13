788171b7f15b13244240d18f1af7262e5c2c18a7
Wkan <wangkan1119@gmail.com>
2024-07-13 16:53:22 +0800

    example add icon preview from other package

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:iconfont_convert_example/icons/test_icons.dart';
+import 'package:other_package/icons/other_test_icons.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentTabIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentTabIndex = index;
+    });
+  }
+
+  final _previewPages = <Widget>[
+    const TestIconsPreview(),
+    const OtherTestIconsPreview(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +34,22 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const TestIconsPreview(),
+      home: Scaffold(
+          body: _previewPages[_currentTabIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _currentTabIndex,
+            onTap: _onItemTapped,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(TestIcons.appstore),
+                label: 'Example Icon',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(OtherTestIcons.faxian),
+                label: 'Other Package Icon',
+              ),
+            ],
+          )),
     );
   }
 }
